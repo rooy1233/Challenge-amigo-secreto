c47964eb52b7e962b78725cd9e3fffa27633e9b8
Rodrigo Cota <roy_robin.sed@hotmail.com>
2025-08-18 22:36:20 -0300

    terminado

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-
+let listaDeAmigos = [];
 
 function agregarAmigo() {
     let amigos = document.getElementById('amigo').value;
     if (amigos === '') {
         alert ('Por favor, ingrese un nombre válido');
     } else {
-        lista ('h3', amigos);
+        listaDeAmigos.push (amigos);
+        let agregarAmigos = document.getElementById("listaAmigos");
+        let contenido = "";
+        for (let amigo of listaDeAmigos){
+            contenido += `<li>${amigo}</li>`;
+            agregarAmigos.innerHTML = contenido;
+        }
     }
-    
     limpiarCaja ();
     return
 }
@@ -27,3 +32,15 @@ function lista (elemento, texto){
     return
 }
 
+function sortearAmigo() {
+    if (listaDeAmigos == ""){
+        alert(":)");  
+    }else {
+        let indiceAleatorio = Math.floor(Math.random()*listaDeAmigos.length);
+        let amigoAleatorio = listaDeAmigos[indiceAleatorio];
+        console.log(indiceAleatorio);
+        console.log(amigoAleatorio);
+        let resultadoSorteo = document.getElementById('resultado');
+        resultadoSorteo.innerHTML = `Felicidades ${amigoAleatorio} es el amigo secreto.`;  
+    }
+}
